29322218972157ab2b670b5f299b40d1831803d2
NULL568 <null568@outlook.com>
2024-10-06 19:20:18 +0200

    Beta v1.5

--- PrimeXor.lua
+++ PrimeXor.lua
@@ -9,6 +9,12 @@ client.Chatted:Connect(function(msg)
 	UIS.InputBegan:Connect(function(key)
 		if key.KeyCode == Enum.KeyCode.T then
 			hrp.CFrame = players:FindFirstChild(msg).Character.HumanoidRootPart.CFrame
+		elseif key.KeyCode == Enum.KeyCode.R then
+			for _, R in pairs(game:GetDescendants()) do
+				if R:IsA("RemoteEvent") then
+					R:FireServer()
+				end
+			end
 		end
 	end)
 end)
