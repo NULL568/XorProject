90a92f1050051c28f8cb5234cdcf059996059f09
NULL568 <null568@outlook.com>
2024-10-29 08:27:42 +0100

    saves

--- PrimeXor.lua
+++
@@ -1,9 +0,0 @@
-local UIS = game:GetService("UserInputService")
-local plrs = game:GetService("Players")
-local client = plrs.LocalPlayer
-local char = client.Character or client.CharacterAdded:Wait()
-local hrp = char:WaitForChild("HumanoidRootPart")
-
-client.Chatted:Connect(function(msg)
-	if msg == ""
-end)
